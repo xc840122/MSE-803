5af904048bd2f9f50f78307aac7b2c7a8d1a77a3
Chi Xu <xuchi007@hotmail.com>
2025-02-13 12:59:00 +1300

    week9 basic-line-plot

---
+++ week-9/basic-line-plot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 25, 30, 40]
+plt.plot(x, y, marker='o')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Basic Line Plot')
+plt.show()
